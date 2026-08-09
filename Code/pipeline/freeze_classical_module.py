16226395805e7374dcd6ffda71e2eb1d1baa9798
Vickey2212 <vigneshwaran.palanisamy@students.cdu.edu.au>
2026-08-09 20:14:52 +0930

    Update model state dict loading path

--- Code/pipeline/freeze_classical_module.py
+++ Code/pipeline/freeze_classical_module.py
@@ -1,5 +1,5 @@
 model = ClassicalFDIABinary(N_BUSES, EDGE_LIST_1BASED).to(DEVICE)
-model.load_state_dict(torch.load("G:/My Drive/PhD/Implementation/RQ1/Simulation/IEEE-30-30/best_classical_end2end_binary.pt", map_location=DEVICE))
+model.load_state_dict(torch.load(OUTPUT_DIR / "best_classical_end2end_binary.pt", map_location=DEVICE))
 model.eval()
 
 for p in model.parameters():
