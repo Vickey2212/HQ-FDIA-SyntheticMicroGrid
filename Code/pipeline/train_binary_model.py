d4e33b7475a85080ea0a01702eb3235872f8b694
Vickey2212 <vigneshwaran.palanisamy@students.cdu.edu.au>
2026-08-17 14:28:16 +0930

    Prepare target tensor and clean up gradient check
    Uncommented the line that prepares the target tensor 'y' for training and removed gradient check print statement.

--- Code/pipeline/train_binary_model.py
+++ Code/pipeline/train_binary_model.py
@@ -9,7 +9,7 @@ def train_embed_epoch_binary(model, loader, opt, loss_fn):
     for x, y in tqdm(loader, leave=False):
 
         x = x.to(DEVICE)
-        # y = y.to(DEVICE).float().unsqueeze(1)  # 
+        # y = y.to(DEVICE).float().unsqueeze(1)  
         y = y.to(DEVICE)
 
         opt.zero_grad()
@@ -20,10 +20,9 @@ def train_embed_epoch_binary(model, loader, opt, loss_fn):
 
         loss.backward()
 
-        # gradient check (only first batch)
+  
         for name, param in model.named_parameters():
             if param.grad is not None:
-                # print("Grad check:", name, param.grad.abs().mean().item())
                 break
 
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
